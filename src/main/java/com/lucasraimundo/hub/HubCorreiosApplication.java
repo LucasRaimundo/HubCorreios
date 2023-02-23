76c10237db9b07af5694d41f74352e1308b2cbf2
Lucas Raimundo <lucasraimundo2010@gmail.com>
2023-02-23 12:02:12 -0300

    Project created

---
+++ src/main/java/com/lucasraimundo/hub/HubCorreiosApplication.java
@@ -0,0 +1,13 @@
+package com.lucasraimundo.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubCorreiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubCorreiosApplication.class, args);
+	}
+
+}
